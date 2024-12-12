f6dfec38c7432d953043af3445434cdc7d581248
avgnpc <5025221059@student.its.ac.id>
2024-12-12 16:07:43 +0700

    fea: update admin view

---
+++ frontend/app/admin/payment/page.tsx
@@ -0,0 +1,152 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import LogoutButton from '@/app/components/LogoutButton';
+import Sidebar from '@/app/components/Sidebar';
+import { Navbar, NavbarContent, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Button } from "@nextui-org/react";
+import { useRouter } from 'next/navigation';
+
+export default function PaymentAdmin() {
+  const [user, setUser] = useState<{ data: { email: string; name: string } } | null>(null);
+  const [payments, setPayments] = useState<any[]>([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const getTokenFromCookies = () => {
+      const cookies = document.cookie.split('; ');
+      const tokenCookie = cookies.find((cookie) => cookie.startsWith('auth-token='));
+      return tokenCookie ? tokenCookie.split('=')[1] : null;
+    };
+
+    // Fetch user data from API
+    const fetchUserData = async () => {
+      try {
+        const token = getTokenFromCookies();
+
+        if (!token) {
+          throw new Error('No authentication token found in cookies.');
+        }
+
+        const response = await fetch('http://localhost:3000/api/v1/auth/me', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error('Failed to fetch user data');
+        }
+        const data = await response.json();
+        setUser(data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    // Fetch payment data from API
+    const fetchPaymentData = async () => {
+      try {
+        const token = getTokenFromCookies();
+
+        if (!token) {
+          throw new Error('No authentication token found in cookies.');
+        }
+
+        const response = await fetch('http://localhost:3000/api/v1/payment/', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error('Failed to fetch payment data');
+        }
+        const data = await response.json();
+        setPayments(data.data.payments);
+      } catch (error) {
+        console.error('Error fetching payment data:', error);
+      }
+    };
+
+    fetchUserData();
+    fetchPaymentData();
+  }, []);
+
+  const handleLogout = () => {
+    LogoutButton();
+  };
+
+  return (
+    <>
+      <Navbar maxWidth="full">
+        <NavbarContent as="div" justify="end">
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <Avatar
+                showFallback
+                isBordered
+                as="button"
+                className="transition-transform"
+                color="secondary"
+                name={user ? user.data.name : "Guest"}
+                size="sm"
+                src="https://images.unsplash.com/broken"
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem key="profile" className="h-14 gap-2">
+                <p className="font-semibold">Signed in as</p>
+                <p className="font-semibold">{user?.data.email}</p>
+              </DropdownItem>
+              <DropdownItem key="logout" color="danger" onClick={handleLogout}>
+                Log Out
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </NavbarContent>
+      </Navbar>
+
+      <div className="min-h-screen bg-gray-200 text-gray-800">
+        <main className="container flex gap-8">
+          <Sidebar />
+          <div className="flex-1">
+            <div className="bg-white w-full shadow-sm"></div>
+
+            <div className='flex-1 w-full'>
+              <div className="p-8 flex justify-between">
+                <h1 className="text-3xl font-bold text-black">Payments</h1>
+              </div>
+
+              {payments.length > 0 ? (
+                <Table className="mx-8 max-w-5xl">
+                  <TableHeader>
+                    <TableColumn>Id</TableColumn>
+                    <TableColumn>Payment Date</TableColumn>
+                    <TableColumn>Account Number</TableColumn>
+                    <TableColumn>Status</TableColumn>
+                    <TableColumn>Transaction ID</TableColumn>
+                  </TableHeader>
+                  <TableBody>
+                    {payments.map((payment) => (
+                      <TableRow key={payment.id}>
+                        <TableCell className="py-4">{payment.id}</TableCell>
+                        <TableCell className="py-4">{new Date(payment.payment_date).toLocaleString()}</TableCell>
+                        <TableCell className="py-4">{payment.account_number}</TableCell>
+                        <TableCell className="py-4">{payment.status}</TableCell>
+                        <TableCell className="py-4">{payment.transaction_id}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              ) : (
+                <p>No Payments</p>
+              )}
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
---
+++ frontend/app/admin/transaction/page.tsx
@@ -0,0 +1,204 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import LogoutButton from "@/app/components/LogoutButton";
+import Sidebar from "@/app/components/Sidebar";
+import {
+  Navbar,
+  NavbarContent,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Avatar,
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  Button,
+} from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+
+export const VerticalDotsIcon = ({ size = 24, width, height, ...props }) => {
+  return (
+    <svg
+      aria-hidden="true"
+      fill="none"
+      focusable="false"
+      height={size || height}
+      role="presentation"
+      viewBox="0 0 24 24"
+      width={size || width}
+      {...props}
+    >
+      <path
+        d="M12 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 12c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
+
+export default function TransactionAdmin() {
+  const [user, setUser] = useState<{ data: { email: string; name: string } } | null>(null);
+  const [transactions, setTransactions] = useState<any[]>([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const getTokenFromCookies = () => {
+      const cookies = document.cookie.split("; ");
+      const tokenCookie = cookies.find((cookie) => cookie.startsWith("auth-token="));
+      return tokenCookie ? tokenCookie.split("=")[1] : null;
+    };
+
+    // Fetch user data from API
+    const fetchUserData = async () => {
+      try {
+        const token = getTokenFromCookies();
+
+        if (!token) {
+          throw new Error("No authentication token found in cookies.");
+        }
+
+        const response = await fetch("http://localhost:3000/api/v1/auth/me", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+        const data = await response.json();
+        setUser(data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    // Fetch transaction data from API
+    const fetchTransactionData = async () => {
+      try {
+        const token = getTokenFromCookies();
+
+        if (!token) {
+          throw new Error("No authentication token found in cookies.");
+        }
+
+        const response = await fetch("http://localhost:3000/api/v1/transaction/", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Failed to fetch transaction data");
+        }
+        const data = await response.json();
+        setTransactions(data.data.transactions);
+      } catch (error) {
+        console.error("Error fetching transaction data:", error);
+      }
+    };
+
+    fetchUserData();
+    fetchTransactionData();
+  }, []);
+
+  const handleLogout = () => {
+    LogoutButton();
+  };
+
+  return (
+    <>
+      <Navbar maxWidth="full">
+        <NavbarContent as="div" justify="end">
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <Avatar
+                showFallback
+                isBordered
+                as="button"
+                className="transition-transform"
+                color="secondary"
+                name={user ? user.data.name : "Guest"}
+                size="sm"
+                src="https://images.unsplash.com/broken"
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem key="profile" className="h-14 gap-2">
+                <p className="font-semibold">Signed in as</p>
+                <p className="font-semibold">{user?.data.email}</p>
+              </DropdownItem>
+              <DropdownItem key="logout" color="danger" onClick={handleLogout}>
+                Log Out
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </NavbarContent>
+      </Navbar>
+
+      <div className="min-h-screen bg-gray-200 text-gray-800">
+        <main className="container flex gap-8">
+          <Sidebar />
+          <div className="flex-1">
+            <div className="p-8 flex justify-between">
+              <h1 className="text-3xl font-bold text-black">Transactions</h1>
+            </div>
+
+            {transactions.length > 0 ? (
+              <Table className="mx-8 max-w-5xl">
+                <TableHeader>
+                  <TableColumn>ID</TableColumn>
+                  <TableColumn>Transaction Date</TableColumn>
+                  <TableColumn>Total Quantity</TableColumn>
+                  <TableColumn>Total Price</TableColumn>
+                  <TableColumn>Status</TableColumn>
+                  <TableColumn>User Name</TableColumn>
+                  <TableColumn>User Email</TableColumn>
+                  <TableColumn>Action</TableColumn>
+                </TableHeader>
+                <TableBody>
+                  {transactions.map((transaction) => (
+                    <TableRow key={transaction.id}>
+                      <TableCell>{transaction.id}</TableCell>
+                      <TableCell>
+                        {new Date(transaction.transaction_date).toLocaleString()}
+                      </TableCell>
+                      <TableCell>{transaction.total_quantity}</TableCell>
+                      <TableCell>{transaction.total_price}</TableCell>
+                      <TableCell>{transaction.status}</TableCell>
+                      <TableCell>{transaction.user.name}</TableCell>
+                      <TableCell>{transaction.user.email}</TableCell>
+                      <TableCell>
+                        <div className="relative flex justify-end items-center gap-2">
+                          <Dropdown className="bg-background border-1 border-default-200">
+                            <DropdownTrigger>
+                              <Button isIconOnly radius="full" size="sm" variant="light">
+                                <VerticalDotsIcon className="text-default-400" width={20} height={20} />
+                              </Button>
+                            </DropdownTrigger>
+                            <DropdownMenu>
+                              <DropdownItem key="edit">View Details</DropdownItem>
+                            </DropdownMenu>
+                          </Dropdown>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            ) : (
+              <p className="mx-8">No Transactions Found</p>
+            )}
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
